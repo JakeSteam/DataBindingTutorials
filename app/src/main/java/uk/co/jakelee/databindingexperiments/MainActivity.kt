e5ca6bea1d11a60c1de3a10064b4a7da5fc8d8fa
Jake Lee <github@jakelee.co.uk>
2019-07-23 21:15:14 +0100

    Project creation

---
+++ app/src/main/java/uk/co/jakelee/databindingexperiments/MainActivity.kt
@@ -0,0 +1,12 @@
+package uk.co.jakelee.databindingexperiments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
