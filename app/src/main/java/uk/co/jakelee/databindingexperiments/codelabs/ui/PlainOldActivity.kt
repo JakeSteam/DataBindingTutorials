86694f90dff9f02f588f683461c376d79fd4b3fe
Jake Lee <github@jakelee.co.uk>
2019-07-23 22:05:39 +0100

    Step 7 completed

--- app/src/main/java/uk/co/jakelee/databindingexperiments/codelabs/ui/PlainOldActivity.kt
+++ app/src/main/java/uk/co/jakelee/databindingexperiments/codelabs/ui/PlainOldActivity.kt
@@ -1,20 +1,10 @@
 package uk.co.jakelee.databindingexperiments.codelabs.ui
 
-import android.content.Context
-import android.content.res.ColorStateList
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.widget.ImageViewCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import uk.co.jakelee.databindingexperiments.R
-import uk.co.jakelee.databindingexperiments.codelabs.data.Popularity
 import uk.co.jakelee.databindingexperiments.codelabs.data.SimpleViewModel
 import uk.co.jakelee.databindingexperiments.databinding.CodelabsPlainActivityBinding
 
@@ -24,58 +14,16 @@ import uk.co.jakelee.databindingexperiments.databinding.CodelabsPlainActivityBin
 class PlainOldActivity : AppCompatActivity() {
 
     // Obtain ViewModel from ViewModelProviders
-    private val viewModel by lazy { ViewModelProviders.of(this).get(SimpleViewModel::class.java) }
+    private val viewModel by lazy {
+        ViewModelProviders.of(this).get(SimpleViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding : CodelabsPlainActivityBinding = DataBindingUtil.setContentView(this, R.layout.codelabs_plain_activity)
+
+        binding.lifecycleOwner = this
         binding.viewmodel = viewModel
-
-        // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
-        updateLikes()
-    }
-
-    /**
-     * This method has many problems:
-     * - It's calling findViewById multiple times
-     * - It has untestable logic
-     * - It's updating two views when called even if they're not changing
-     */
-    private fun updateLikes() {
-        findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
-        findViewById<ProgressBar>(R.id.progressBar).progress =
-            (viewModel.likes * 100 / 5).coerceAtMost(100)
-        val image = findViewById<ImageView>(R.id.imageView)
-
-        val color = getAssociatedColor(viewModel.popularity, this)
-
-        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
-
-        image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
-    }
-
-    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
-        return when (popularity) {
-            Popularity.NORMAL -> context.theme.obtainStyledAttributes(
-                intArrayOf(android.R.attr.colorForeground)
-            ).getColor(0, 0x000000)
-            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
-            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
-        }
-    }
-
-    private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
-        return when (popularity) {
-            Popularity.NORMAL -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_person_black_96dp)
-            }
-            Popularity.POPULAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-            Popularity.STAR -> {
-                ContextCompat.getDrawable(context, R.drawable.ic_whatshot_black_96dp)
-            }
-        }
     }
 }
