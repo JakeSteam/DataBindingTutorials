8ad8031a766e0a016f7e4d090051f769d3741c12
Jake Lee <github@jakelee.co.uk>
2019-07-28 20:38:10 +0100

    Data binding completed

--- app/src/main/java/uk/co/jakelee/databindingexperiments/androidwave/AndroidWaveActivity.kt
+++ app/src/main/java/uk/co/jakelee/databindingexperiments/androidwave/AndroidWaveActivity.kt
@@ -12,5 +12,7 @@ class AndroidWaveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: AndroidwaveActivityBinding = DataBindingUtil.setContentView(this, R.layout.androidwave_activity)
+
+        binding.user = User("Mr User", "101 Fake Road | England", "101K", "29")
     }
 }
