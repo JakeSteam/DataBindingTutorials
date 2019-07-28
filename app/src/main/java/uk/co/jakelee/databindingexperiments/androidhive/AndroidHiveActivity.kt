717d1eb0b022b372c231b5f9f4776e4e4d617cc5
Jake Lee <github@jakelee.co.uk>
2019-07-28 18:30:02 +0100

    Step 1.5 completed

--- app/src/main/java/uk/co/jakelee/databindingexperiments/androidhive/AndroidHiveActivity.kt
+++ app/src/main/java/uk/co/jakelee/databindingexperiments/androidhive/AndroidHiveActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import uk.co.jakelee.databindingexperiments.R
 import uk.co.jakelee.databindingexperiments.databinding.AndroidhiveActivityBinding
+import android.widget.Toast
+import android.content.Context
+import android.view.View
 
 
 class AndroidHiveActivity : AppCompatActivity() {
@@ -14,11 +17,32 @@ class AndroidHiveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: AndroidhiveActivityBinding = DataBindingUtil.setContentView(this, R.layout.androidhive_activity)
+        val binding: AndroidhiveActivityBinding = DataBindingUtil.setContentView(this, uk.co.jakelee.databindingexperiments.R.layout.androidhive_activity)
 
         setSupportActionBar(binding.toolbar);
 
         user = User("Ravi Tamada", "ravi@androidhive.info")
         binding.user = user
+        binding.content.handlers = MyClickHandlers(this)
+    }
+
+    inner class MyClickHandlers(internal var context: Context) {
+
+        fun onFabClicked(view: View) {
+            Toast.makeText(applicationContext, "FAB clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        fun onButtonClick(view: View) {
+            Toast.makeText(applicationContext, "Button clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        fun onButtonClickWithParam(view: View, user: User) {
+            Toast.makeText(applicationContext, "Button clicked! Name: " + user.name, Toast.LENGTH_SHORT).show()
+        }
+
+        fun onButtonLongPressed(view: View): Boolean {
+            Toast.makeText(applicationContext, "Button long pressed!", Toast.LENGTH_SHORT).show()
+            return false
+        }
     }
 }
