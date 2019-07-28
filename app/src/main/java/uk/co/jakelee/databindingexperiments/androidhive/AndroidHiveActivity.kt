024139d604703efdfad783a88ff4bd247f667fa7
Jake Lee <github@jakelee.co.uk>
2019-07-28 18:15:06 +0100

    Step 1.4 completed

--- app/src/main/java/uk/co/jakelee/databindingexperiments/androidhive/AndroidHiveActivity.kt
+++ app/src/main/java/uk/co/jakelee/databindingexperiments/androidhive/AndroidHiveActivity.kt
@@ -1,10 +1,10 @@
 package uk.co.jakelee.databindingexperiments.androidhive
 
-import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import uk.co.jakelee.databindingexperiments.databinding.AndroidhiveActivityBinding
+import androidx.databinding.DataBindingUtil
 import uk.co.jakelee.databindingexperiments.R
+import uk.co.jakelee.databindingexperiments.databinding.AndroidhiveActivityBinding
 
 
 class AndroidHiveActivity : AppCompatActivity() {
@@ -14,9 +14,11 @@ class AndroidHiveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: uk.co.jakelee.databindingexperiments.databinding.AndroidhiveActivityBinding = DataBindingUtil.setContentView(this, R.layout.androidhive_activity)
+        val binding: AndroidhiveActivityBinding = DataBindingUtil.setContentView(this, R.layout.androidhive_activity)
+
+        setSupportActionBar(binding.toolbar);
 
         user = User("Ravi Tamada", "ravi@androidhive.info")
-        binding.setUser(user)
+        binding.user = user
     }
 }
